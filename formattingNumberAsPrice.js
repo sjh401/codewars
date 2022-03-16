b0d6c78194ad6ffafe86684e20f79648db4e24a9
stephenharrity <sjh401@gmail.com>
2022-03-16 10:08:50 -0500

    formatting number as price

--- formattingNumberAsPrice.js
+++ formattingNumberAsPrice.js
@@ -1,17 +1,13 @@
 var numberToPrice = function(number) {
-    console.log(number)
-    let periods = number.toString().match(/[.]/g)
-    if(Number.isInteger(parseInt(number)) === false) return 'NaN'
-    if(periods && number.toString().match(/[.]/g).length > 1) return 'NaN'
-    number = parseFloat(number).toFixed(3)
-    let arr = number.toString().split('')
-    arr.pop()
-    arr.reverse()
-    for(let i=3; i<arr.length; i+=1){
-      if((i-2)%3 === 0 && arr[i+1] !== '-' && i+1 !==arr.length){
-        arr[i] = ','.concat(arr[i])
-      }
+  if(typeof number != 'number') return "NaN"
+  number = parseFloat(number).toFixed(3)
+  let arr = number.toString().split('')
+  arr.pop()
+  arr.reverse()
+  for(let i=3; i<arr.length; i+=1){
+    if((i-2)%3 === 0 && arr[i+1] !== '-' && i+1 !==arr.length){
+      arr[i] = ','.concat(arr[i])
     }
-    return arr.reverse().join('')
-    
+  }
+  return arr.reverse().join('')
   }
