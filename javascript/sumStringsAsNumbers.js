3030bd4d937412baee93b3fbdb3a37fcdc90f64e
stephenharrity <sjh401@gmail.com>
2022-10-24 13:47:40 -0500

    Sum Strings as Numbers

--- javascript/sumStringsAsNumbers.js
+++ javascript/sumStringsAsNumbers.js
@@ -7,36 +7,36 @@ function sumStrings(a,b) {
     let overNine = 0;
   
     while(i < a.length || i < b.length){
-      let temp = ''
+      let temp = '';
       if(i >= a.length){
         temp = b[i] * 1;
       } else if(i >= b.length){
         temp = a[i] * 1;
       } else{
         temp = (a[i]*1 + b[i]*1);
-      }
+      };
       
       if(overNine > 0){
         temp += overNine;
         overNine = 0;
-      }
-      temp = temp.toString().split('')
+      };
+      temp = temp.toString().split('');
       
       if(temp.length > 1){
-        totalString = temp[1].concat(totalString)
-        overNine = temp[0]*1
+        totalString = temp[1].concat(totalString);
+        overNine = temp[0]*1;
       } else {
-        totalString = temp[0].concat(totalString)
-      }
-    i+=1
-    }
+        totalString = temp[0].concat(totalString);
+      };
+    i+=1;
+    };
     if(overNine > 0){
-      totalString = overNine.toString().concat(totalString)
-    }
+      totalString = overNine.toString().concat(totalString);
+    };
     
     totalString = totalString.split('');
     while(totalString[0] === "0"){
       totalString.shift();
-    }
-    return totalString.join('')
+    };
+    return totalString.join('');
   }
